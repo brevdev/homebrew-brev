462af939fc43c62758a72e11f61b14222d62d5b3
r33drichards <57335981+r33drichards@users.noreply.github.com>
2022-02-03 21:14:27 +0000

    update version

--- Formula/brev.rb
+++ Formula/brev.rb
@@ -1,8 +1,8 @@
 class Brev < Formula
   desc "CLI tool for managing workspaces provided by brev.dev"
   homepage "https://docs.brev.dev"
-  url "https://github.com/brevdev/brev-cli/archive/refs/tags/v0.6.14.tar.gz"
-  sha256 "f4c2c335526dc2eb8ad7e4d3e6d348d6094d9c4279708b498f4a5af6543b5b2d"
+  url "https://github.com/brevdev/brev-cli/archive/refs/tags/v0.6.15.tar.gz"
+  sha256 "4a27d652a54c58e40b61f2ebd050f434854e99ae20ef4edb150339011a1124d5"
   license "MIT"
   depends_on "go" => :build
 
