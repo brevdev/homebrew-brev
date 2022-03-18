a12954fc0e1e4a3e53414099e7815920c7dfa83e
r33drichards <57335981+r33drichards@users.noreply.github.com>
2022-03-18 04:02:44 +0000

    bumpb

--- Formula/brev.rb
+++ Formula/brev.rb
@@ -1,9 +1,8 @@
 class Brev < Formula
   desc "CLI tool for managing workspaces provided by brev.dev"
   homepage "https://docs.brev.dev"
-  url "https://github.com/brevdev/brev-cli/archive/refs/tags/v0.6.30.tar.gz"
-  sha256 "4c64c45997fe626d65ed11183716aaf721e1bc4e4bf2b26d39073144e177ad6a"
-
+  url "https://github.com/brevdev/brev-cli/archive/refs/tags/v0.6.31.tar.gz"
+  sha256 "7b17e3c6a0e3fd3082f7bcd9919bc934ec44278acd7ad61b9a33f7e920236e43"
   license "MIT"
   depends_on "go" => :build
 
